0741eeb3d64a4303e951964459aa16c7ebb69aa6
AsataVG <vgorodok845@gmail.com>
2020-12-17 01:18:19 +0200

    Changes

--- server/router/main.ts
+++ server/router/main.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import { getUrl } from '../controllers/auxiliary';
 const router = express.Router();
 router.get('*', (req, res) => {
-    // res.send('ok')
-    res.sendFile(getUrl(__dirname, '../dist/index.html'));
+    res.sendFile(getUrl(__dirname, '../../../dist/index.html'));
 });
 
 export { router as main };
