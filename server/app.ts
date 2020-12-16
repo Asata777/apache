a34b0ec22f8435762782bd7965baa5e3f7250d6e
AsataVG <vgorodok845@gmail.com>
2020-12-16 23:54:14 +0200

    Changes

--- server/app.ts
+++ server/app.ts
@@ -10,6 +10,7 @@ import socketBestmafiaConnection from './controllers/socket';
 import mongoose from 'mongoose';
 import config from 'config';
 // import MongoStore from 'connect-mongo';
+
 import { getUrl, KeepAlive } from './controllers/auxiliary';
 import { bestmafia } from './router/bestmafia';
 import { main } from './router/main';
