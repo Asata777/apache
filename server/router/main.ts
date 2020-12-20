385fc65c47175a6abe16f52c51ef41e7ad95ffc7
AsataVG <vgorodok845@gmail.com>
2020-12-20 16:56:46 +0200

    fds

--- server/router/main.ts
+++ server/router/main.ts
@@ -3,7 +3,7 @@ import { getUrl } from '../controllers/auxiliary';
 const router = express.Router();
 router.get('*', (req, res) => {
     // res.sendFile(getUrl(__dirname, '../../dist/index.html'));
-    res.send('takoyevdfg')
+    res.send('takoyevdf')
 });
 
 export { router as main };
