174f24370d2f94be2739e514e580a32a66d125f7
= <=>
2020-12-20 17:29:26 +0200

    fds

--- server/router/main.ts
+++ server/router/main.ts
@@ -3,7 +3,7 @@ import { getUrl } from '../controllers/auxiliary';
 const router = express.Router();
 router.get('*', (req, res) => {
     // res.sendFile(getUrl(__dirname, '../../dist/index.html'));
-    res.send('takovsdf')
+    res.send('takvsdf')
 });
 
 export { router as main };
