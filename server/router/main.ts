831386462c6fc35fb908de2c03a0ec55fc270813
= <=>
2020-12-21 01:18:56 +0200

    'ch'

--- server/router/main.ts
+++ server/router/main.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import { getUrl } from '../controllers/auxiliary';
 const router = express.Router();
 router.get('*', (req, res) => {
-    res.sendFile(getUrl(__dirname, '../../dist/index.html'));
-    // res.send('test')
+    // res.sendFile(getUrl(__dirname, '../../dist/index.html'));
+    res.send('testsa')
 });
 
 export { router as main };
