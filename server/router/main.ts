e3b2ace062190acc6ffb63d04cdf25442f12873c
= <=>
2020-12-20 17:49:27 +0200

    changes

--- server/router/main.ts
+++ server/router/main.ts
@@ -3,7 +3,7 @@ import { getUrl } from '../controllers/auxiliary';
 const router = express.Router();
 router.get('*', (req, res) => {
     // res.sendFile(getUrl(__dirname, '../../dist/index.html'));
-    res.send('taksf')
+    res.send('taks')
 });
 
 export { router as main };
