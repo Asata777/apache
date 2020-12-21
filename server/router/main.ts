ea9efb3aa68112ccfa52abb094c8c4c28daa40e6
Igor <radeckiy777@gmail.com>
2020-12-22 01:10:01 +0200

    ch

--- server/router/main.ts
+++ server/router/main.ts
@@ -3,8 +3,8 @@ import { getUrl } from '../controllers/auxiliary';
 import path from 'path';
 const router = express.Router();
 router.get('*', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../../dist/index.html'));
-    res.send('Nu rabotayet zhe')
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+    // res.send('Nu rabotayet zhe')
 });
 
 export { router as main };
