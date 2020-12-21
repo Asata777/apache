16ba344cc66a74e38c1de047b6b4770102d354f1
Igor <radeckiy777@gmail.com>
2020-12-21 02:00:31 +0200

    changes

--- server/router/main.ts
+++ server/router/main.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getUrl } from '../controllers/auxiliary';
+import path from 'path';
 const router = express.Router();
 router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'))
     // res.sendFile(getUrl(__dirname, '../../dist/index.html'));
-    res.send('testsa')
+    // res.send('Хахахахахха')
 });
 
 export { router as main };
